def6e7422faca4c70ca0799d92a378bd2254df94
Ankit_kumar <ankit95001kumar@gmail.com>
2025-03-06 00:02:04 +0530

    [Ankit] Two Way Data Binding Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -11,6 +12,7 @@ export class AppComponent {
   title = 'FirstProject';
   imageUrl:string = "image.png"
   url:string="https://www.bridgelabz.com/";
+  name:String="";
 
   
   message:String = "Hello from BridgeLabz"
